e126a3b6b16b9eaf7c40398bbdc19cee33978b4f
Bruno Matušić <bmatusic22@student.foi.hr>
2026-05-30 11:53:09 +0200

    Dodani osnovni modeli (RadniNalog, StavkaNaloga, Korisnik)

---
+++ Models/RadniNalog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServisNalogPro.Models
+{
+    public class RadniNalog
+    {
+        public int IdNaloga { get; set; }
+        public string OpisKvara { get; set; }
+        public DateTime Datum { get; set; }
+        public string Status{ get; set; }
+        public int IdZaposlenika { get; set; }
+    }
+}
---
+++ Models/StavkaNaloga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServisNalogPro.Models
+{
+    public class StavkaNaloga
+    {
+        public int IdStavke { get; set; }
+        public string Opis { get; set; }
+        public decimal Cijena { get; set; }
+        public int IdNaloga { get; set; }
+    }
+}
